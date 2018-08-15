35a82039f1b2d0d2ad7be29a8e2c8bf7071de940
Carmon Colvin <9137759+CarmonColvin@users.noreply.github.com>
2018-08-15 14:19:38 -0500

    Created basic controllers.

---
+++ DemoWebApiUnityLog4Net/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace DemoWebApiUnityLog4Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public String Index()
+        {
+            return @"You have reached HomeController.Index().";
+        }
+    }
+}
---
+++ DemoWebApiUnityLog4Net/Controllers/MainApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+
+namespace DemoWebApiUnityLog4Net.Controllers
+{
+    public class MainApiController : ApiController
+    {
+        /// <summary>
+        /// GET: api/MainApi/
+        /// </summary>
+        /// <returns></returns>
+        public String Get()
+        {
+            return @"You have reached MainApiController.Get()";
+        }
+    }
+}
